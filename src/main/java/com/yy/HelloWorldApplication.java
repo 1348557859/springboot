a1417c4cbc56ee6683d8f4dc63bb560a33642adc
pengyy <1348557859@qq.com>
2019-10-04 11:42:40 +0800

    hot_fix test

--- src/main/java/com/yy/HelloWorldApplication.java
+++ src/main/java/com/yy/HelloWorldApplication.java
@@ -8,6 +8,7 @@ public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hot_fix");
 		SpringApplication.run(HelloWorldApplication.class,args);
 	}
 
